9f6cdf22a257689ffa5b3823a8261302637b1386
Volkova Maria <MaRi221994@yandex.ru>
2025-09-25 12:37:16 +0700

    Знакомство с PageObject. Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,46 +1,12 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
 
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
-
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        context.storage_state(path="browser-state.json")
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json")
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-
-        title = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(title).to_have_text('Courses')
-
-        icon = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(icon).to_be_visible()
-
-        text = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(text).to_have_text('There is no results')
-
-        description_text = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(description_text).to_have_text('Results from the load test pipeline will be displayed here')
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.fill_registration_form('user.name@gmail.com', 'username', 'password')
+    registration_page.click_registration_button()
+    dashboard_page.check_visible_and_text_title()
